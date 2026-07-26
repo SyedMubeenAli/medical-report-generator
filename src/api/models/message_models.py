2a9380937601addec0e1ac6481a34ba8e053975e
Mubeen <mubeenali2k17@gmail.com>
2026-07-26 15:23:51 +0500

    Clean repository and update gitignore

--- src/api/models/message_models.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-
-class MessageResponse(BaseModel):
-
-    message: str
-    report_id: str
--- src/generators/utils.py
+++
@@ -1,9 +0,0 @@
-import random
-
-
-def random_float(low, high, digits=1):
-    return round(random.uniform(low, high), digits)
-
-
-def random_int(low, high):
-    return random.randint(low, high)
